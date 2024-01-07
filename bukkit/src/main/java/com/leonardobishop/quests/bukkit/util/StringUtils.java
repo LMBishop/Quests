a184e76053aa55df4fb5f156fa6c5c0ecb8f811d
Steven Van Ingelgem <steven@vaningelgem.be>
2024-01-07 08:04:10 +0100

    Also allow '_' and '-' to be used as "characters".

--- bukkit/src/main/java/com/leonardobishop/quests/bukkit/util/StringUtils.java
+++ bukkit/src/main/java/com/leonardobishop/quests/bukkit/util/StringUtils.java
@@ -5,7 +5,6 @@ package com.leonardobishop.quests.bukkit.util;
  * https://github.com/apache/commons-lang/blob/master/LICENSE.txt
  */
 public class StringUtils {
-
     public static boolean isEmpty(final CharSequence cs) {
         return cs == null || cs.length() == 0;
     }
@@ -14,13 +13,8 @@ public class StringUtils {
         if (isEmpty(cs)) {
             return false;
         }
-        final int sz = cs.length();
-        for (int i = 0; i < sz; i++) {
-            if (!Character.isLetterOrDigit(cs.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
+
+        return cs.chars().allMatch(c -> Character.isLetterOrDigit(c) || c == '_' || c == '-');
     }
 
     public static boolean isNumeric(final CharSequence cs) {
