b70b0b228cbc92ae58846b4417023d7deff239d6
Krakenied <Krakenied1@gmail.com>
2024-02-15 14:17:41 +0100

    Add ServerNPC and ZNPCsPlus interact task types too

--- bukkit/src/main/java/com/leonardobishop/quests/bukkit/tasktype/type/dependent/CitizensInteractTaskType.java
+++ bukkit/src/main/java/com/leonardobishop/quests/bukkit/tasktype/type/dependent/CitizensInteractTaskType.java
@@ -1,80 +1,32 @@
 package com.leonardobishop.quests.bukkit.tasktype.type.dependent;
 
 import com.leonardobishop.quests.bukkit.BukkitQuestsPlugin;
-import com.leonardobishop.quests.bukkit.tasktype.BukkitTaskType;
 import com.leonardobishop.quests.bukkit.util.TaskUtils;
-import com.leonardobishop.quests.bukkit.util.chat.Chat;
-import com.leonardobishop.quests.common.config.ConfigProblem;
-import com.leonardobishop.quests.common.player.QPlayer;
-import com.leonardobishop.quests.common.player.questprogressfile.TaskProgress;
-import com.leonardobishop.quests.common.quest.Quest;
 import com.leonardobishop.quests.common.quest.Task;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 import net.citizensnpcs.api.npc.NPC;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
-public final class CitizensInteractTaskType extends BukkitTaskType {
+public final class CitizensInteractTaskType extends InteractTaskType<Integer> {
 
     private final BukkitQuestsPlugin plugin;
 
     public CitizensInteractTaskType(BukkitQuestsPlugin plugin) {
-        super("citizens_interact", TaskUtils.TASK_ATTRIBUTION_STRING, "Interact with an NPC to complete the quest.");
+        super("citizens_interact", TaskUtils.TASK_ATTRIBUTION_STRING, "Interact with a Citizens NPC to complete the quest.");
         this.plugin = plugin;
 
-        super.addConfigValidator((config, problems) -> {
-            if (config.containsKey("npc-name") && config.containsKey("npc-id")) {
-                problems.add(new ConfigProblem(ConfigProblem.ConfigProblemType.WARNING,
-                        "Both npc-name and npc-id is specified; npc-name will be ignored", null, "npc-name"));
-            }
-        });
+        super.addConfigValidator(TaskUtils.useIntegerConfigValidator(this, "npc-id"));
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onNPCRightClick(NPCRightClickEvent event) {
-        Player player = event.getClicker();
-        if (player.hasMetadata("NPC")) {
-            return;
-        }
-
-        QPlayer qPlayer = plugin.getPlayerManager().getPlayer(player.getUniqueId());
-        if (qPlayer == null) {
-            return;
-        }
-
         NPC npc = event.getNPC();
-        String npcName = null;
+        handle(event.getClicker(), npc.getId(), npc.getName(), plugin);
+    }
 
-        for (TaskUtils.PendingTask pendingTask : TaskUtils.getApplicableTasks(player, qPlayer, this)) {
-            Quest quest = pendingTask.quest();
-            Task task = pendingTask.task();
-            TaskProgress taskProgress = pendingTask.taskProgress();
-
-            super.debug("Player clicked NPC", quest.getId(), task.getId(), player.getUniqueId());
-
-            Integer configNPCId = (Integer) task.getConfigValue("npc-id");
-            if (configNPCId != null) {
-                if (npc.getId() != configNPCId) {
-                    super.debug("NPC id " + npc.getId() + " does not match required id, continuing...", quest.getId(), task.getId(), player.getUniqueId());
-                    continue;
-                }
-            } else {
-                String configNPCName = (String) task.getConfigValue("npc-name");
-                if (configNPCName != null) {
-                    if (npcName == null) {
-                        npcName = Chat.legacyStrip(Chat.legacyColor(npc.getName()));
-                    }
-
-                    if (!configNPCName.equals(npcName)) {
-                        super.debug("NPC name " + npcName + " does not match required name, continuing...", quest.getId(), task.getId(), player.getUniqueId());
-                        continue;
-                    }
-                }
-            }
-
-            super.debug("Marking task as complete", quest.getId(), task.getId(), player.getUniqueId());
-            taskProgress.setCompleted(true);
-        }
+    @Override
+    public Integer getNPCId(Task task) {
+        return (Integer) task.getConfigValue("npc-id");
     }
 }
---
+++ bukkit/src/main/java/com/leonardobishop/quests/bukkit/tasktype/type/dependent/InteractTaskType.java
@@ -0,0 +1,78 @@
+package com.leonardobishop.quests.bukkit.tasktype.type.dependent;
+
+import com.leonardobishop.quests.bukkit.BukkitQuestsPlugin;
+import com.leonardobishop.quests.bukkit.tasktype.BukkitTaskType;
+import com.leonardobishop.quests.bukkit.util.TaskUtils;
+import com.leonardobishop.quests.bukkit.util.chat.Chat;
+import com.leonardobishop.quests.common.config.ConfigProblem;
+import com.leonardobishop.quests.common.player.QPlayer;
+import com.leonardobishop.quests.common.player.questprogressfile.TaskProgress;
+import com.leonardobishop.quests.common.quest.Quest;
+import com.leonardobishop.quests.common.quest.Task;
+import org.bukkit.entity.Player;
+
+public abstract class InteractTaskType<T> extends BukkitTaskType {
+
+    public InteractTaskType(String type, String author, String description) {
+        super(type, author, description);
+
+        super.addConfigValidator((config, problems) -> {
+            if (config.containsKey("npc-name") && config.containsKey("npc-id")) {
+                problems.add(new ConfigProblem(ConfigProblem.ConfigProblemType.WARNING,
+                        "Both npc-name and npc-id is specified; npc-name will be ignored", null, "npc-name"));
+            }
+        });
+    }
+
+    public abstract T getNPCId(Task task);
+
+    public void handle(Player player, T npcId, String npcName, BukkitQuestsPlugin plugin) {
+        if (!player.isOnline()) {
+            return;
+        }
+
+        QPlayer qPlayer = plugin.getPlayerManager().getPlayer(player.getUniqueId());
+        if (qPlayer == null) {
+            return;
+        }
+
+        boolean nameCorrected = false;
+
+        for (TaskUtils.PendingTask pendingTask : TaskUtils.getApplicableTasks(player, qPlayer, this)) {
+            Quest quest = pendingTask.quest();
+            Task task = pendingTask.task();
+            TaskProgress taskProgress = pendingTask.taskProgress();
+
+            super.debug("Player clicked NPC", quest.getId(), task.getId(), player.getUniqueId());
+
+            T configNPCId = getNPCId(task);
+            if (configNPCId != null) {
+                if (!npcId.equals(configNPCId)) {
+                    super.debug("NPC id " + npcId + " does not match required id, continuing...", quest.getId(), task.getId(), player.getUniqueId());
+                    continue;
+                }
+            } else {
+                String configNPCName = (String) task.getConfigValue("npc-name");
+                if (configNPCName != null) {
+                    if (npcName == null) {
+                        super.debug("NPC name is empty and does not match required name, continuing...", quest.getId(), task.getId(), player.getUniqueId());
+                        continue;
+                    }
+
+                    if (!nameCorrected) {
+                        npcName = Chat.legacyStrip(Chat.legacyColor(npcName));
+                        nameCorrected = true;
+                    }
+
+                    if (!configNPCName.equals(npcName)) {
+                        super.debug("NPC name " + npcName + " does not match required name, continuing...", quest.getId(), task.getId(), player.getUniqueId());
+                        continue;
+                    }
+                }
+            }
+
+            super.debug("Marking task as complete", quest.getId(), task.getId(), player.getUniqueId());
+            taskProgress.setCompleted(true);
+        }
+    }
+}
