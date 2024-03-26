7a15a6b667c7eb09fa9b5cac677fd48c85c9b63a
Krakenied <Krakenied1@gmail.com>
2024-03-26 04:30:58 +0100

    Add resurrecting task type

---
+++ bukkit/src/main/java/com/leonardobishop/quests/bukkit/tasktype/type/ResurrectingTaskType.java
@@ -0,0 +1,65 @@
+package com.leonardobishop.quests.bukkit.tasktype.type;
+
+import com.leonardobishop.quests.bukkit.BukkitQuestsPlugin;
+import com.leonardobishop.quests.bukkit.tasktype.BukkitTaskType;
+import com.leonardobishop.quests.bukkit.util.TaskUtils;
+import com.leonardobishop.quests.bukkit.util.constraint.TaskConstraintSet;
+import com.leonardobishop.quests.common.player.QPlayer;
+import com.leonardobishop.quests.common.player.questprogressfile.TaskProgress;
+import com.leonardobishop.quests.common.quest.Quest;
+import com.leonardobishop.quests.common.quest.Task;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityResurrectEvent;
+
+public final class ResurrectingTaskType extends BukkitTaskType {
+
+    private final BukkitQuestsPlugin plugin;
+
+    public ResurrectingTaskType(BukkitQuestsPlugin plugin) {
+        super("resurrecting", TaskUtils.TASK_ATTRIBUTION_STRING, "Resurrect a set amount of times.");
+        this.plugin = plugin;
+
+        super.addConfigValidator(TaskUtils.useRequiredConfigValidator(this, "amount"));
+        super.addConfigValidator(TaskUtils.useIntegerConfigValidator(this, "amount"));
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onEntityResurrect(EntityResurrectEvent event) {
+        LivingEntity entity = event.getEntity();
+        if (!(entity instanceof Player player)) {
+            return;
+        }
+
+        if (player.hasMetadata("NPC")) {
+            return;
+        }
+
+        QPlayer qPlayer = plugin.getPlayerManager().getPlayer(player.getUniqueId());
+        if (qPlayer == null) {
+            return;
+        }
+
+        for (TaskUtils.PendingTask pendingTask : TaskUtils.getApplicableTasks(player, qPlayer, this, TaskConstraintSet.ALL)) {
+            Quest quest = pendingTask.quest();
+            Task task = pendingTask.task();
+            TaskProgress taskProgress = pendingTask.taskProgress();
+
+            super.debug("Player resurrected", quest.getId(), task.getId(), player.getUniqueId());
+
+            int progress = TaskUtils.incrementIntegerTaskProgress(taskProgress);
+            super.debug("Incrementing task progress (now " + progress + ")", quest.getId(), task.getId(), player.getUniqueId());
+
+            int amount = (int) task.getConfigValue("amount");
+
+            if (progress >= amount) {
+                super.debug("Marking task as complete", quest.getId(), task.getId(), player.getUniqueId());
+                taskProgress.setCompleted(true);
+            }
+
+            TaskUtils.sendTrackAdvancement(player, quest, task, pendingTask, amount);
+        }
+    }
+}
