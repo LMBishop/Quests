LMBishop/Quests

commit e5c0237bcb45c5d308e0a2b763f50ac97135c768

Add support for Folia

bukkit/src/main/java/com/leonardobishop/quests/bukkit/BukkitQuestsPlugin.java
@@ -30,6 +30,10 @@ import com.leonardobishop.quests.bukkit.menu.itemstack.QItemStackRegistry;
 import com.leonardobishop.quests.bukkit.questcompleter.BukkitQuestCompleter;
 import com.leonardobishop.quests.bukkit.questcontroller.NormalQuestController;
 import com.leonardobishop.quests.bukkit.runnable.QuestsAutoSaveRunnable;
+import com.leonardobishop.quests.bukkit.scheduler.WrappedTask;
+import com.leonardobishop.quests.bukkit.scheduler.bukkit.BukkitServerSchedulerAdapter;
+import com.leonardobishop.quests.bukkit.scheduler.ServerScheduler;
+import com.leonardobishop.quests.bukkit.scheduler.folia.FoliaServerScheduler;
 import com.leonardobishop.quests.bukkit.storage.MySqlStorageProvider;
 import com.leonardobishop.quests.bukkit.storage.YamlStorageProvider;
 import com.leonardobishop.quests.bukkit.tasktype.BukkitTaskTypeManager;
@@ -46,7 +50,6 @@ import com.leonardobishop.quests.common.plugin.Quests;
 import com.leonardobishop.quests.common.quest.QuestCompleter;
 import com.leonardobishop.quests.common.quest.QuestManager;
 import com.leonardobishop.quests.common.questcontroller.QuestController;
-import com.leonardobishop.quests.common.scheduler.ServerScheduler;
 import com.leonardobishop.quests.common.storage.StorageProvider;
 import com.leonardobishop.quests.common.tasktype.TaskType;
 import com.leonardobishop.quests.common.tasktype.TaskTypeManager;
@@ -60,7 +63,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -100,8 +102,8 @@ public class BukkitQuestsPlugin extends JavaPlugin implements Quests {
     private VersionSpecificHandler versionSpecificHandler;
 
     private LogHistory logHistory;
-    private BukkitTask questAutoSaveTask;
-    private BukkitTask questQueuePollTask;
+    private QuestsAutoSaveRunnable questAutoSaveTask;
+    private WrappedTask questQueuePollTask;
     private BiFunction<Player, String, String> placeholderAPIProcessor;
 
     @Override
@@ -162,7 +164,9 @@ public class BukkitQuestsPlugin extends JavaPlugin implements Quests {
         this.generateConfigurations();
         this.questsConfig = new BukkitQuestsConfig(new File(super.getDataFolder() + File.separator + "config.yml"));
         this.questManager = new QuestManager(this);
-        this.serverScheduler = new BukkitServerSchedulerAdapter(this);
+
+        this.serverScheduler = FoliaServerScheduler.FOLIA ? new FoliaServerScheduler(this) : new BukkitServerSchedulerAdapter(this);
+        questsLogger.info("Running server scheduler: " + serverScheduler.getServerSchedulerName());
 
         // Load base configuration for use during rest of startup procedure
         if (!this.reloadBaseConfiguration()) {
@@ -271,7 +275,7 @@ public class BukkitQuestsPlugin extends JavaPlugin implements Quests {
         super.getServer().getPluginManager().registerEvents(new PlayerLeaveListener(this), this);
 
         // Register task types after the server has fully started
-        Bukkit.getScheduler().runTask(this, () -> {
+        getScheduler().doSync(() -> {
             // Setup external plugin hooks
             if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
                 this.placeholderAPIHook = new PlaceholderAPIHook();
@@ -448,6 +452,8 @@ public class BukkitQuestsPlugin extends JavaPlugin implements Quests {
         try {
             qPlayerManager.getStorageProvider().shutdown();
         } catch (Exception ignored) { }
+
+        serverScheduler.cancelAllTasks();
     }
 
     @Override
@@ -502,7 +508,7 @@ public class BukkitQuestsPlugin extends JavaPlugin implements Quests {
             long autoSaveInterval = this.getConfig().getLong("options.performance-tweaking.quest-autosave-interval", 12000);
             try {
                 if (questAutoSaveTask != null) questAutoSaveTask.cancel();
-                questAutoSaveTask = Bukkit.getScheduler().runTaskTimer(this, () -> new QuestsAutoSaveRunnable(this), autoSaveInterval, autoSaveInterval);
+                questAutoSaveTask = new QuestsAutoSaveRunnable(this, autoSaveInterval);
             } catch (Exception ex) {
                 questsLogger.debug("Cannot cancel and restart quest autosave task");
             }
@@ -510,7 +516,7 @@ public class BukkitQuestsPlugin extends JavaPlugin implements Quests {
             long queueExecuteInterval = this.getConfig().getLong("options.performance-tweaking.quest-queue-executor-interval", 1);
             try {
                 if (questQueuePollTask != null) questQueuePollTask.cancel();
-                questQueuePollTask = Bukkit.getScheduler().runTaskTimer(this, (BukkitQuestCompleter) questCompleter, queueExecuteInterval, queueExecuteInterval);
+                questQueuePollTask = serverScheduler.runTaskTimer((BukkitQuestCompleter) questCompleter, queueExecuteInterval, queueExecuteInterval);
             } catch (Exception ex) {
                 questsLogger.debug("Cannot cancel and restart queue executor task");
             }

bukkit/build.gradle
@@ -55,6 +55,10 @@ dependencies {
     compileOnly('io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT') {
         exclude(group: 'it.unimi.dsi', module: 'fastutil') // exclude fastutil just to don't use it (for 1.8 support)
     }
+    // Folia
+    compileOnly('dev.folia:folia-api:1.20.1-R0.1-SNAPSHOT') {
+        exclude(group: 'it.unimi.dsi', module: 'fastutil') // exclude fastutil just to don't use it (for 1.8 support)
+    }
     // ASkyBlock
     compileOnly('com.wasteofplastic:askyblock:3.0.9.4') { transitive = false }
     // AuthLib

bukkit/src/main/java/com/leonardobishop/quests/bukkit/BukkitServerSchedulerAdapter.java
@@ -1,22 +1,0 @@
-package com.leonardobishop.quests.bukkit;
-
-import com.leonardobishop.quests.common.scheduler.ServerScheduler;
-
-public class BukkitServerSchedulerAdapter implements ServerScheduler {
-
-    private final BukkitQuestsPlugin plugin;
-
-    public BukkitServerSchedulerAdapter(BukkitQuestsPlugin plugin) {
-        this.plugin = plugin;
-    }
-
-    @Override
-    public void doSync(Runnable runnable) {
-        plugin.getServer().getScheduler().runTask(plugin, runnable);
-    }
-
-    @Override
-    public void doAsync(Runnable runnable) {
-        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, runnable);
-    }
-}

bukkit/src/main/java/com/leonardobishop/quests/bukkit/command/AdminUpdateCommandHandler.java
@@ -21,7 +21,7 @@ public class AdminUpdateCommandHandler implements CommandHandler {
     @Override
     public void handle(CommandSender sender, String[] args) {
         sender.sendMessage(ChatColor.GRAY + "Checking for updates...");
-        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+        plugin.getScheduler().doAsync(() -> {
             plugin.getUpdater().check();
             if (plugin.getUpdater().isUpdateReady()) {
                 Messages.QUEST_UPDATER.send(sender,

bukkit/src/main/java/com/leonardobishop/quests/bukkit/hook/papi/QuestsPlaceholders.java
@@ -285,7 +285,7 @@ public class QuestsPlaceholders extends PlaceholderExpansion implements Cacheabl
             final Map<String, String> map = new HashMap<>();
             map.put(params, result);
             cache.put(player, map);
-            Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, () -> cache.get(player).remove(params), plugin.getConfig().getInt("options.placeholder-cache-time", 10) * 20L);
+            plugin.getScheduler().runTaskLaterAsynchronously(() -> cache.get(player).remove(params), plugin.getConfig().getInt("options.placeholder-cache-time", 10) * 20L);
         }
         return result;
     }

bukkit/src/main/java/com/leonardobishop/quests/bukkit/listener/PlayerJoinListener.java
@@ -28,12 +28,12 @@ public class PlayerJoinListener implements Listener {
                     .replace("{newver}", plugin.getUpdater().getReturnedVersion())
                     .replace("{oldver}", plugin.getUpdater().getInstalledVersion())
                     .replace("{link}", plugin.getUpdater().getUpdateLink());
-            Bukkit.getScheduler().runTaskLater(this.plugin, () -> event.getPlayer().sendMessage(updateMessage), 50L);
+            plugin.getScheduler().runTaskLaterAtEntity(event.getPlayer(), () -> event.getPlayer().sendMessage(updateMessage), 50L);
         }
 
         final Player player = event.getPlayer();
         plugin.getQuestsLogger().debug("PlayerJoinListener: " + player.getUniqueId() + " (" + player.getName() + ")");
-        plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
+        plugin.getScheduler().runTaskLater(() -> {
             if (!player.isOnline()) return;
             plugin.getPlayerManager().loadPlayer(player.getUniqueId()).thenAccept(qPlayer -> {
                 if (qPlayer == null) return;

bukkit/src/main/java/com/leonardobishop/quests/bukkit/questcontroller/NormalQuestController.java
@@ -222,7 +222,7 @@ public class NormalQuestController implements QuestController {
             PlayerFinishQuestEvent questFinishEvent = new PlayerFinishQuestEvent(player, qPlayer, questProgress, questFinishMessage);
             Bukkit.getPluginManager().callEvent(questFinishEvent);
             // PlayerFinishQuestEvent -- end
-            Bukkit.getServer().getScheduler().runTask(plugin, () -> {
+            plugin.getScheduler().doSync(() -> {
                 for (String s : quest.getRewards()) {
                     s = s.replace("{player}", player.getName());
                     if (plugin.getConfig().getBoolean("options.quests-use-placeholderapi")) {

bukkit/src/main/java/com/leonardobishop/quests/bukkit/runnable/QuestsAutoSaveRunnable.java
@@ -1,27 +1,27 @@
 package com.leonardobishop.quests.bukkit.runnable;
 
 import com.leonardobishop.quests.bukkit.BukkitQuestsPlugin;
+import com.leonardobishop.quests.bukkit.scheduler.WrappedRunnable;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.UUID;
 
-public class QuestsAutoSaveRunnable extends BukkitRunnable {
+public class QuestsAutoSaveRunnable extends WrappedRunnable {
 
     private final Queue<UUID> queue = new LinkedList<>();
     private final BukkitQuestsPlugin plugin;
 
-    public QuestsAutoSaveRunnable(BukkitQuestsPlugin plugin) {
+    public QuestsAutoSaveRunnable(BukkitQuestsPlugin plugin, long autoSaveInterval) {
         for (Player player : Bukkit.getOnlinePlayers()) {
             queue.add(player.getUniqueId());
         }
 
         this.plugin = plugin;
 
-        this.runTaskTimer(plugin, 2L, 2L);
+        this.runTaskTimer(plugin.getScheduler(), autoSaveInterval, autoSaveInterval);
     }
 
     @Override

commit 73a50058682eeb65f936c01eebf348beca1a6270

Fix auto saving feature due to regression caused by https://github.com/LMBishop/Quests/commit/e5c0237bcb45c5d308e0a2b763f50ac97135c768

bukkit/src/main/java/com/leonardobishop/quests/bukkit/BukkitQuestsPlugin.java
@@ -602,7 +602,7 @@ public class BukkitQuestsPlugin extends JavaPlugin implements Quests {
             long autoSaveInterval = this.getConfig().getLong("options.performance-tweaking.quest-autosave-interval", 12000);
             try {
                 if (questAutoSaveTask != null) questAutoSaveTask.cancel();
-                questAutoSaveTask = new QuestsAutoSaveRunnable(this).runTaskTimer(getScheduler(), autoSaveInterval, autoSaveInterval);
+                questAutoSaveTask = serverScheduler.runTaskTimer(() -> new QuestsAutoSaveRunnable(this), autoSaveInterval, autoSaveInterval);
             } catch (Exception ex) {
                 questsLogger.debug("Cannot cancel and restart quest autosave task");
             }