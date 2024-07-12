816e0c73a1ac708ed4cad05c85be4d928db9d927
Krakenied <Krakenied1@gmail.com>
2024-07-12 12:29:11 +0200

    Clean up task type manager Mark some TaskType methods as final as they aren't really meant to be overridden

--- common/src/main/java/com/leonardobishop/quests/common/tasktype/TaskTypeManager.java
+++ common/src/main/java/com/leonardobishop/quests/common/tasktype/TaskTypeManager.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.UUID;
 import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
@@ -22,38 +23,47 @@ import java.util.function.Supplier;
 @SuppressWarnings("UnusedReturnValue")
 public abstract class TaskTypeManager {
 
-    private final Map<String, TaskType> taskTypes = new HashMap<>();
-    private final Map<String, String> aliases = new HashMap<>();
     private final Set<String> exclusions;
+    private final Map<String, TaskType> taskTypes;
+    private final Map<String, String> aliases;
+    private boolean registrationsOpen;
     private int registered;
     private int skipped;
     private int unsupported;
-    private boolean registrationsOpen;
-
-    public TaskTypeManager() {
-        this.registrationsOpen = true;
-        this.exclusions = Collections.emptySet();
-    }
-
-    public TaskTypeManager(final @NotNull Set<String> exclusions) {
-        this.registrationsOpen = true;
-        this.exclusions = exclusions;
-    }
 
     /**
-     * Closes the task type registrations. This is typically done after start-up.
-     */
-    public void closeRegistrations() {
-        this.registrationsOpen = false;
-    }
-
-    /**
-     * Checks if registrations are still open.
+     * Constructs a TaskTypeManager with a specified set of exclusions.
      *
-     * @return true if registrations are open, false otherwise
+     * @param exclusions a set of task type names to exclude from registration
      */
-    public boolean areRegistrationsOpen() {
-        return this.registrationsOpen;
+    public TaskTypeManager(final @NotNull Set<String> exclusions) {
+        Objects.requireNonNull(exclusions, "exclusions cannot be null");
+
+        this.exclusions = exclusions;
+        this.taskTypes = new HashMap<>();
+        this.aliases = new HashMap<>();
+        this.registrationsOpen = true;
+        this.registered = 0;
+        this.skipped = 0;
+        this.unsupported = 0;
+    }
+
+    /**
+     * Constructs a TaskTypeManager with an empty set of exclusions.
+     * Use this constructor when no exclusions are needed.
+     */
+    public TaskTypeManager() {
+        this(Set.of());
+    }
+
+    /**
+     * Returns an immutable set containing all task type exclusions.
+     *
+     * @return immutable {@link Set} containing all task type exclusions
+     */
+    @SuppressWarnings("unused")
+    public @NotNull Set<String> getExclusions() {
+        return Collections.unmodifiableSet(this.exclusions);
     }
 
     /**
@@ -63,93 +73,6 @@ public abstract class TaskTypeManager {
      */
     public @NotNull Collection<TaskType> getTaskTypes() {
         return Collections.unmodifiableCollection(this.taskTypes.values());
-    }
-
-    /**
-     * Resets all quest to task type registrations. This does not clear the task types registered to the task type manager.
-     */
-    public void resetTaskTypes() {
-        for (final TaskType taskType : this.taskTypes.values()) {
-            taskType.unregisterAll();
-        }
-    }
-
-    /**
-     * Registers a task type with the task type manager.
-     *
-     * @param taskType the task type to register
-     * @return true if the task type was successfully registered, false otherwise
-     */
-    public boolean registerTaskType(final @NotNull TaskType taskType) {
-        Objects.requireNonNull(taskType, "taskType cannot be null");
-
-        if (!this.registrationsOpen) {
-            throw new IllegalStateException("No longer accepting new task types (must be done before quests are loaded)");
-        }
-
-        final String type = taskType.getType();
-        final Set<String> aliasTypes = taskType.getAliases();
-
-        if (this.exclusions.contains(type) || this.taskTypes.containsKey(type)
-                || !Collections.disjoint(this.exclusions, aliasTypes)
-                || !Collections.disjoint(this.taskTypes.keySet(), aliasTypes)
-                || !Collections.disjoint(this.aliases.keySet(), aliasTypes)) {
-            this.skipped++;
-            return false;
-        }
-
-        this.taskTypes.put(type, taskType);
-        for (final String alias : taskType.getAliases()) {
-            this.aliases.put(alias, type);
-        }
-
-        this.registered++;
-        return true;
-    }
-
-    /**
-     * Registers a task type with the task type manager using suppliers.
-     *
-     * @param taskTypeSupplier supplier of the task type to register
-     * @param compatibilitySuppliers suppliers to check for task type compatibility
-     * @return true if the task type was successfully registered, false otherwise
-     */
-    public boolean registerTaskType(final @NotNull Supplier<TaskType> taskTypeSupplier, final @NotNull BooleanSupplier @NotNull ... compatibilitySuppliers) {
-        Objects.requireNonNull(taskTypeSupplier, "taskTypeSupplier cannot be null");
-
-        if (!this.registrationsOpen) {
-            throw new IllegalStateException("No longer accepting new task types (must be done before quests are loaded)");
-        }
-
-        for (final BooleanSupplier supplier : compatibilitySuppliers) {
-            if (!supplier.getAsBoolean()) {
-                this.unsupported++;
-                return false;
-            }
-        }
-
-        return this.registerTaskType(taskTypeSupplier.get());
-    }
-
-    /**
-     * Registers a quest with its task types. This will register the quest to each task type it contains.
-     *
-     * @param quest the quest to register
-     */
-    public void registerQuestTasksWithTaskTypes(final @NotNull Quest quest) {
-        Objects.requireNonNull(quest, "quest cannot be null");
-
-        if (this.registrationsOpen) {
-            throw new IllegalStateException("Still accepting new task types (type registrations must be closed before registering quests)");
-        }
-
-        for (final Task task : quest.getTasks()) {
-            final TaskType taskType = this.getTaskType(task.getType());
-
-            if (taskType != null) {
-                taskType.registerQuest(quest);
-            }
-        }
     }
 
     /**
@@ -189,12 +112,77 @@ public abstract class TaskTypeManager {
     }
 
     /**
-     * Returns an immutable set containing all task type exclusions.
+     * Registers a task type with the task type manager.
      *
-     * @return immutable {@link Set} containing all task type exclusions
+     * @param taskType the task type to register
+     * @return true if the task type was successfully registered, false otherwise
      */
-    public @NotNull Set<String> getExclusions() {
-        return Collections.unmodifiableSet(this.exclusions);
+    public boolean registerTaskType(final @NotNull TaskType taskType) {
+        Objects.requireNonNull(taskType, "taskType cannot be null");
+
+        if (!this.registrationsOpen) {
+            throw new IllegalStateException("No longer accepting new task types (must be done before quests are loaded)");
+        }
+
+        final String type = taskType.getType();
+        final Set<String> aliases = taskType.getAliases();
+
+        if (this.exclusions.contains(type) || this.taskTypes.containsKey(type)
+                || !Collections.disjoint(this.exclusions, aliases)
+                || !Collections.disjoint(this.taskTypes.keySet(), aliases)
+                || !Collections.disjoint(this.aliases.keySet(), aliases)) {
+            this.skipped++;
+            return false;
+        }
+
+        this.taskTypes.put(type, taskType);
+        for (final String alias : taskType.getAliases()) {
+            this.aliases.put(alias, type);
+        }
+
+        this.registered++;
+        return true;
+    }
+
+    /**
+     * Registers a task type with the task type manager using suppliers.
+     *
+     * @param taskTypeSupplier supplier of the task type to register
+     * @param compatibilitySuppliers suppliers to check for task type compatibility
+     * @return true if the task type was successfully registered, false otherwise
+     */
+    public boolean registerTaskType(final @NotNull Supplier<TaskType> taskTypeSupplier, final @NotNull BooleanSupplier @NotNull ... compatibilitySuppliers) {
+        Objects.requireNonNull(taskTypeSupplier, "taskTypeSupplier cannot be null");
+        Objects.requireNonNull(compatibilitySuppliers, "compatibilitySuppliers cannot be null");
+
+        if (!this.registrationsOpen) {
+            throw new IllegalStateException("No longer accepting new task types (must be done before quests are loaded)");
+        }
+
+        for (final BooleanSupplier supplier : compatibilitySuppliers) {
+            if (!supplier.getAsBoolean()) {
+                this.unsupported++;
+                return false;
+            }
+        }
+
+        return this.registerTaskType(taskTypeSupplier.get());
+    }
+
+    /**
+     * Closes the task type registrations. This is typically done after start-up.
+     */
+    public void closeRegistrations() {
+        this.registrationsOpen = false;
+    }
+
+    /**
+     * Checks if registrations are still open.
+     *
+     * @return true if registrations are open, false otherwise
+     */
+    public boolean areRegistrationsOpen() {
+        return this.registrationsOpen;
     }
 
     /**
@@ -223,4 +211,36 @@ public abstract class TaskTypeManager {
     public int getUnsupported() {
         return this.unsupported;
     }
+
+    /**
+     * Registers a quest with its task types. This will register the quest to each task type it contains.
+     *
+     * @param quest the quest to register
+     */
+    public void registerQuestTasksWithTaskTypes(final @NotNull Quest quest) {
+        Objects.requireNonNull(quest, "quest cannot be null");
+
+        if (this.registrationsOpen) {
+            throw new IllegalStateException("Still accepting new task types (type registrations must be closed before registering quests)");
+        }
+
+        for (final Task task : quest.getTasks()) {
+            final TaskType taskType = this.getTaskType(task.getType());
+
+            if (taskType != null) {
+                taskType.registerQuest(quest);
+            }
+        }
+    }
+
+    /**
+     * Resets all quest to task type registrations. This does not clear the task types registered to the task type manager.
+     */
+    public void resetTaskTypes() {
+        for (final TaskType taskType : this.taskTypes.values()) {
+            taskType.unregisterAll();
+        }
+    }
+
+    public abstract void sendDebug(final @NotNull String message, final @NotNull String taskType, final @NotNull String questId, final @NotNull String taskId, final @NotNull UUID associatedPlayer);
 }
