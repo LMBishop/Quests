9f4cd182676dda55f93882cdc57e94a436f60ef5
Krakenied <krakenied1@gmail.com>
2024-11-16 03:02:34 +0100

    Add hidden option to quest options

--- bukkit/src/main/java/com/leonardobishop/quests/bukkit/menu/StartedQMenu.java
+++ bukkit/src/main/java/com/leonardobishop/quests/bukkit/menu/StartedQMenu.java
@@ -6,6 +6,7 @@ import com.leonardobishop.quests.bukkit.menu.element.QuestMenuElement;
 import com.leonardobishop.quests.bukkit.util.chat.Chat;
 import com.leonardobishop.quests.common.player.QPlayer;
 import com.leonardobishop.quests.common.quest.Quest;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,22 +14,26 @@ import java.util.List;
 /**
  * Represents a menu listing quests a player has started.
  */
-public class StartedQMenu extends PaginatedQMenu {
+public final class StartedQMenu extends PaginatedQMenu {
 
-    public StartedQMenu(BukkitQuestsPlugin plugin, QPlayer owner, List<Quest> quests) {
+    public StartedQMenu(final @NotNull BukkitQuestsPlugin plugin, final @NotNull QPlayer owner, final @NotNull List<Quest> quests) {
         super(owner, Chat.legacyColor(plugin.getQuestsConfig().getString("options.guinames.quests-started-menu")),
                 plugin.getQuestsConfig().getBoolean("options.trim-gui-size.quests-started-menu"), 54, plugin);
 
-        List<MenuElement> elements = new ArrayList<>();
+        final List<MenuElement> elements = new ArrayList<>();
 
-        for (Quest quest : quests) {
+        for (final Quest quest : quests) {
+            if (quest.isHidden()) {
+                continue;
+            }
+
             if (!owner.hasStartedQuest(quest)) {
                 continue;
             }
+
             elements.add(new QuestMenuElement(plugin, quest, this));
         }
 
-        super.populate("custom-elements.started", elements, null);
+        this.populate("custom-elements.started", elements, null);
     }
-
 }
