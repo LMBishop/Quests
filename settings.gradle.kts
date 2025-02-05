75a2d7f90984b21d62b0332d691be21845208a8b
Krakenied <krakenied1@gmail.com>
2025-02-05 19:53:13 +0100

    Update Gradle Wrapper to 8.12.1 Update jvmdg to 1.2.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
     plugins {
         id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
         id("io.github.goooler.shadow") version "8.1.8"
-        id("xyz.wagyourtail.jvmdowngrader") version "1.2.1"
+        id("xyz.wagyourtail.jvmdowngrader") version "1.2.2"
     }
 }
 
