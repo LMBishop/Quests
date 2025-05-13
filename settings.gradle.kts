9cce6b1d004a8eaebd21ce6a81b0b6092baf8d82
Krakenied <krakenied1@gmail.com>
2025-05-13 18:12:22 +0200

    Upgrade foojay resolver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     }
 
     plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
         id("io.github.goooler.shadow") version "8.1.8"
         id("xyz.wagyourtail.jvmdowngrader") version "1.2.2"
     }
