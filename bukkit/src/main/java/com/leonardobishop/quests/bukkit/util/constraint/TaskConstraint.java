4f25d8cfc11b7ab70e02ccd73ec4db3648253523
Krakenied <krakenied1@gmail.com>
2026-01-21 12:21:02 +0100

    Replace TaskConstraintSet with EnumSet implementation
    Add constraint set to some task types:
    - enchanting
    - essentials_moneyearn
    - expearn
    - NPC interact task types
    - playerpoints_earn

--- bukkit/src/main/java/com/leonardobishop/quests/bukkit/util/constraint/TaskConstraint.java
+++ bukkit/src/main/java/com/leonardobishop/quests/bukkit/util/constraint/TaskConstraint.java
@@ -1,16 +1,6 @@
 package com.leonardobishop.quests.bukkit.util.constraint;
 
 public enum TaskConstraint {
-    WORLD(0b00000001),
-    BIOME(0x00000010);
-
-    private final int value;
-
-    TaskConstraint(final int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return this.value;
-    }
+    WORLD,
+    BIOME
 }
